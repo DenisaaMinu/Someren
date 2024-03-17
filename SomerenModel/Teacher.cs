f3fdfbccca026a71cf4dba988d0da0299fd33ea9
Denisa Minu <720645@student.inholland.nl>
2024-03-17 12:32:39 +0100

    added RoomId for teacher

--- SomerenModel/Teacher.cs
+++ SomerenModel/Teacher.cs
@@ -6,6 +6,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Age { get; set; }
-        public string TelephoneNumber { get; set; } 
+        public string TelephoneNumber { get; set; }
+        public int RoomId {  get; set; }
     }
 }
